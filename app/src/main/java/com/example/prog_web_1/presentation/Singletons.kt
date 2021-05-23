46de4ea9b6a568ecc0c184a85ee2b68700e16eff
Sarmini <sarmini.c@gmail.com>
2021-05-24 01:15:08 +0200

    Add Cache

--- app/src/main/java/com/example/prog_web_1/presentation/Singletons.kt
+++ app/src/main/java/com/example/prog_web_1/presentation/Singletons.kt
@@ -1,14 +1,25 @@
 package com.example.prog_web_1.presentation
 
+import com.example.prog_web_1.presentation.PokeApplication.Companion.context
 import com.example.prog_web_1.presentation.api.PokeApi
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 
 class Singletons {
     companion object{
+        var cache = Cache(File(context?.cacheDir,"responses"),  10 * 1024 * 1024)
+
+        val okHttpClient: OkHttpClient = OkHttpClient().newBuilder()
+                .cache(cache)
+                .build()
+
         val pokeApi: PokeApi = Retrofit.Builder()
                 .baseUrl("https://pokeapi.co/api/v2/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
                 .build()
                 .create(PokeApi::class.java)
     }
